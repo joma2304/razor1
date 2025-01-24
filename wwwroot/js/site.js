12437daa4bd71e9381fcbb85ae22023b882d6258
Johan Magnusson <joma2304@student.miun.se>
2025-01-24 14:17:53 +0100

    first commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,7 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+const date = new Date().toISOString().slice(0, 10); //Hämta datum i YYYY-MM-DD
+document.getElementById("date-span").innerHTML = date; // Sätt texten till p-element i header
